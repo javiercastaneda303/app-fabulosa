f965852b354f27b2aefcb237827542a56802afb4
javier_303 <javiercastaneda303@gmail.com>
2020-10-31 12:31:52 +0000

    visualizando los inputs nuevos campos

--- insertandoArticulos.js
+++ insertandoArticulos.js
@@ -71,7 +71,7 @@ let app = new Vue({
       },    
       n_articulo () {
         const articulo = {
-          datos: {nombre_corto:'',nombre_largo:'',nombre:'ccc',descripcion:'ddd',pvp:'',p_coste:'eee',fabricante:'fff',ref_fabicante:'ggg'},
+          datos: {nombre_corto:'a',nombre_largo:'',nombre:'ccc',descripcion:'ddd',pvp:'',p_coste:'eee',fabricante:'fff',ref_fabicante:'ggg'},
           destino:[],
           visible: true
         }
